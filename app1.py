6379cfbc9eb5f97999a674886039bf7cde11325f
viharikagarneedi <garneediviharika95@gmail.com>
2025-07-30 13:48:19 +0530

    uploading python files

---
+++ app1.py
@@ -0,0 +1,6 @@
+#for number in range (0,50):
+number=int(input("Enter a number:"))
+if(number%2==0):
+    print("Even number")
+else:
+    print("odd number")
---
+++ app3.py
@@ -0,0 +1,7 @@
+num=10
+scores=[10,20,30]
+names=["Alexa","siri","jarvis"]
+for score in (scores):
+    print(score)
+for name in  (names):
+    print(name)
